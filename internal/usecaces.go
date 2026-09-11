8ecc58d7de8759790b51e7fa1487dc19ce0804ac
Alex <shchadzinskiaa@gmail.com>
2026-09-11 14:09:08 +0300

    feat:rename scenarios to usecases

--- internal/usecaces.go
+++ internal/usecaces.go
@@ -1,42 +1,44 @@
 package internal
 
-import "petshelter/cli"
+import (
+	cli2 "petshelter/internal/cli"
+)
 
 func ScenarioTakeDog(dogs map[string]Dog) bool {
-	cli.PrintDogList(dogs)
-	cli.Println("Введите кличку собаки: ")
-	nickname := cli.InputValidString()
+	cli2.PrintDogList(dogs)
+	cli2.Println("Введите кличку собаки: ")
+	nickname := cli2.InputValidString()
 	d, ok := FindDog(dogs, nickname)
 	if !ok {
-		cli.Println("Собака с такой кличкой не найдена")
+		cli2.Println("Собака с такой кличкой не найдена")
 		return true
 	}
-	cli.PrintDogInfo(dogs, nickname)
-	cli.Println("Забрать из приюта? (да/нет): ")
-	if cli.ValidYesNo() {
+	cli2.PrintDogInfo(dogs, nickname)
+	cli2.Println("Забрать из приюта? (да/нет): ")
+	if cli2.ValidYesNo() {
 		RemoveDog(dogs, nickname)
-		cli.Println("Собака удалена из общего списка, приюта и поликлиники")
-		cli.PrintShelterInfo(d.Shelter)
-		cli.PrintPoliclinicInfo(d.Policlinic)
+		cli2.Println("Собака удалена из общего списка, приюта и поликлиники")
+		cli2.PrintShelterInfo(d.Shelter)
+		cli2.PrintPoliclinicInfo(d.Policlinic)
 	}
-	cli.Println("Смотреть ещё? (да/нет): ")
-	return cli.ValidYesNo()
+	cli2.Println("Смотреть ещё? (да/нет): ")
+	return cli2.ValidYesNo()
 }
 
 func ScenarioAddDog(dogs map[string]Dog, shelters []Shelter, policlinics []Policlinic) bool {
-	cli.Println("Введите кличку: ")
-	nickname := cli.InputValidString()
-	cli.Println("Введите возраст: ")
-	age := cli.InputValidString()
-	cli.Println("Введите вес: ")
-	weight := cli.InputValidString()
-	cli.Println("Введите дату поступления: ")
-	date := cli.InputValidString()
-	cli.Println("Выберите приют: ")
-	shelterChoice := cli.ValidationReadMenuChoice(0, len(shelters)-1)
+	cli2.Println("Введите кличку: ")
+	nickname := cli2.InputValidString()
+	cli2.Println("Введите возраст: ")
+	age := cli2.InputValidString()
+	cli2.Println("Введите вес: ")
+	weight := cli2.InputValidString()
+	cli2.Println("Введите дату поступления: ")
+	date := cli2.InputValidString()
+	cli2.Println("Выберите приют: ")
+	shelterChoice := cli2.ValidationReadMenuChoice(0, len(shelters)-1)
 	shelter := &shelters[shelterChoice]
-	cli.Println("Выберите поликлинику: ")
-	clinicChoice := cli.ValidationReadMenuChoice(0, len(policlinics)-1)
+	cli2.Println("Выберите поликлинику: ")
+	clinicChoice := cli2.ValidationReadMenuChoice(0, len(policlinics)-1)
 	policlinic := &policlinics[clinicChoice]
 	dog := AddDog(
 		dogs,
@@ -47,8 +49,8 @@ func ScenarioAddDog(dogs map[string]Dog, shelters []Shelter, policlinics []Polic
 		shelter,
 		policlinic,
 	)
-	cli.Println("Собака добавлена:")
-	cli.Println(dog.Nickname)
-	cli.Println("Смотреть ещё? (да/нет): ")
-	return cli.ValidYesNo()
+	cli2.Println("Собака добавлена:")
+	cli2.Println(dog.Nickname)
+	cli2.Println("Смотреть ещё? (да/нет): ")
+	return cli2.ValidYesNo()
 }
