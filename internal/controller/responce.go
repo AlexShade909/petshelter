8ffe2adffa55e773fba29313661a32cc00c761cd
Alex <62323755+AlexShade909@users.noreply.github.com>
2026-09-24 23:46:21 +0300

    feat: work GET /localhost:8080/dogs

---
+++ internal/controller/responce.go
@@ -0,0 +1,15 @@
+package controller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		log.Println("writeJSON:", err)
+	}
+}
